854824d572a24a27520b498b9f19f9a3989c7805
caohuijun2018 <1160364324@qq.com>
2021-03-11 19:43:00 +0800

    添加和后端之间的数据交互，更新和删除

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,49 +1,88 @@
+//import axios from "axios";
 import { useEffect, useState } from "react";
-import './App.css'
-import axios from 'axios'
-
-const url ='http://localhost:3001/persons'
+import "./App.css";
+//import axios from "axios";
+import phoneBookService from "./service/phoneboook";
+//const url = "http://localhost:3001/persons";
 const App = () => {
-  // const [persons, setPersons] = useState([
-  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
-  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  // ]);
+  const [persons, setPersons] = useState([]);
 
-  const [persons,setPersons] = useState([])
-  
-  useEffect( () => {
-    axios.get(url).then(response => {
-      setPersons(response.data)
-    })
-  },[])
-
+  useEffect(() => {
+    phoneBookService.getAll().then((returnPerson) => {
+      setPersons(returnPerson);
+    });
+  }, []);
   const [newName, setNewname] = useState("");
   const [newPhone, setNewphone] = useState("");
   const [filterName, setFiltername] = useState("");
+
   const addPerson = (event) => {
     event.preventDefault();
     const object = {
       //新添加的对象
       name: newName,
       number: newPhone,
-      id: persons.length + 1,
+      //id: persons.length + 1,   //id要自动生成
     };
+    const alreadyExit = persons.some((person) => person.name === object.name); //some返回一bollean型数据
+    if (alreadyExit) {
+      alert(`${newName} is already exited`);
+      const phonePerson = persons.find(
+        //找到需要被替代的人
+        (person) => person.name === newName && person.phone !== newPhone
+      );
 
-    // axios.put(url).then(response => {
-    //   setPersons()
-    // })
-    persons.map((person) =>
-      person.name === newName
-        ? alert(`${newName} is already added to phonebook`)
-        : setPersons(persons.concat(object))
-    );
+      const changePerson = { ...phonePerson, number: newPhone }; //新的对象
 
+      if (phonePerson !== null) {
+        //upatePhone({ ...object, id:phonePerson.id });
+        //这里要注意，在更新电话号码的时候，id仍然为persons中已经存在的人的id，只是将新的电话号码更新到原先的id对应的人下
+        let flag = window.confirm(
+          `${newName} is already exited,do you want to replace it?`
+        );
+        
+        if (flag === true) {
+          upClick(changePerson);
+        }
+      }
+    } else {
+      phoneBookService
+        .create(object)
+        .then((returnPerson) => setPersons(persons.concat(returnPerson)));
+    }
     setNewname("");
     setNewphone("");
   };
 
+  const upClick = (changePerson) => {
+    phoneBookService
+      .updata(changePerson, changePerson.id)
+      .then((returnPerson) => {
+        setPersons(
+          persons.map((person) =>
+            person.id === changePerson.id ? returnPerson : person
+          )
+        );
+      });
+  };
+
+  const delPerson = (person) => {
+    let flag = window.confirm(`Delete ${person.name} ?`);
+    if (flag) {
+      phoneBookService.deleteId(person.id).then(() => {
+        phoneBookService
+          .getAll()
+          .then((returnPerson) => setPersons(returnPerson));
+      });
+      // axios.delete(`http://localhost:3001/persons/${person.id}`).then(() => {
+      //   phoneBookService.getAll()
+      //   .then((response) => setPersons(response));
+
+      //   // let filtedPerson = persons.filter((target) => target.id !== person.id);
+      //   // setPersons(filtedPerson);
+      // });
+    }
+  };
   const handleChangeName = (event) => {
     setNewname(event.target.value);
   };
@@ -54,21 +93,24 @@ const App = () => {
   const handleChangeFilter = (event) => {
     setFiltername(event.target.value);
   };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <h1>Test</h1>
-      <form > 
+      <form>
         filter show with{" "}
-        <input value={filterName} onChange={handleChangeFilter}  className = 'input-test'/>
+        <input
+          value={filterName}
+          onChange={handleChangeFilter}
+          className="input-test"
+        />
       </form>
 
       <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleChangeName} />
-        </div>
-        <div>
           number: <input value={newPhone} onChange={handleChangePhone} />
         </div>
         <div>
@@ -77,17 +119,21 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 
-      {persons
-        .filter((person) =>
-          filterName
-            ? person.name.toLowerCase().includes(filterName.toLowerCase()) 
-            : true
-        )
-        .map((person) => (
-          <p key={person.id}>
-            {person.name} {person.number}
-          </p>
-        ))}
+      {persons &&
+        persons
+          .filter((person) =>
+            filterName
+              ? person.name.toLowerCase().includes(filterName.toLowerCase())
+              : true
+          )
+          .map((person) => (
+            <div>
+              <p key={person.id}>
+                {person.name} {person.number}
+                <button onClick={() => delPerson(person)}>delete</button>
+              </p>
+            </div>
+          ))}
     </div>
   );
 };
---
+++ part2/phonebook/src/service/phoneboook.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+const basrUrl = "http://localhost:3001/persons";
+
+// const getAll = () => {
+//   axios.get(basrUrl).then((response) => {
+//     return response.data;
+//   });
+// };
+const getAll = () => {
+  const request = axios.get(basrUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(basrUrl, newObject);
+
+  return request.then((response) => response.data);
+};
+
+const deleteId = (id) => {
+  return axios.delete(`${basrUrl}/${id}`);
+};
+
+const updata = (object,id) => {
+  const request =  axios.put(`${basrUrl}/${id}`,object) ;
+  return request.then(response => response.data)
+}
+export default {
+  getAll,
+  create,
+  deleteId,
+  updata
+};
