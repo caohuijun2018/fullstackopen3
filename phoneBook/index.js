769d429500235f944d2202be5eea3b02d7d6d301
caohuijun2018 <1160364324@qq.com>
2021-07-02 20:30:52 +0800

    添加错误处理中间件

--- phoneBook/index.js
+++ phoneBook/index.js
@@ -3,9 +3,21 @@ const express = require('express')
 require('dotenv').config()
 const app = express();
 const cors = require('cors');
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
 app.use(cors());
 app.use(express.json())
 app.use(express.static('build'))
+app.use(errorHandler)
 
 const Person = require('./phoneBook')
 
@@ -36,7 +48,7 @@ app.get('/info',(request,response) => {
     response.send(`<p>phoneBook has info for ${phoneBook.length} people</p> <p>${time} </p>`)
 })
 
-app.get('/api/persons/:id',(request,response) => {
+app.get('/api/persons/:id',(request,response,next) => {
     // let id = Number(request.params.id);
     // let phonebook = phoneBook.find(phone => phone.id === id);
     // if(phonebook === null) {
@@ -44,21 +56,29 @@ app.get('/api/persons/:id',(request,response) => {
     // } else {
     //     response.json(phonebook)
     // }
-    Person.findById(request.params.id).then(resultPerson => {
+    Person.findById(request.params.id)
+    .then(resultPerson => {
         response.json(resultPerson)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(request,response) => {
-    let  id = Number(request.params.id);
-    let phonebook = phoneBook.find(phone => phone.id === id);
-    if(phonebook === null) {
-        response.status(404).end()
-    } else {
-    phoneBook = phoneBook.filter(phonebook => phonebook.id !== id);
-    // console.log(phoneBook)
-    response.status(204).end()
-    }
+app.delete('/api/persons/:id',(request,response,next) => {
+    // let  id = Number(request.params.id);
+    // let phonebook = phoneBook.find(phone => phone.id === id);
+    // if(phonebook === null) {
+    //     response.status(404).end()
+    // } else {
+    // phoneBook = phoneBook.filter(phonebook => phonebook.id !== id);
+    // // console.log(phoneBook)
+    // response.status(204).end()
+    // }
+
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(202).end()
+    })
+    .catch(error => next(error))
     
 })
 app.post('/api/persons',(request,response) => {
@@ -81,6 +101,22 @@ app.post('/api/persons',(request,response) => {
     newPerson.save().then(savePerson => {
         response.json(savePerson)
     })
+})
+
+app.put('api/persons/:id',(request,response) => {
+    const body = request.body;
+
+    const upPerson = {
+        name : body.name ,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id,upPerson) 
+        .then(returnPerson => {
+            response.json(returnPerson)
+        })
+        .catch(error => next(error))
+
 })
 
 
